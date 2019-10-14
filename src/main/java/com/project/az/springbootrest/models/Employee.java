331eb45f85f82d1e16661df810ebebc3c924647f
azhwani <azh.abderrahim@gmail.com>
2019-10-14 18:18:50 +0100

    Add Employee Model Class

---
+++ src/main/java/com/project/az/springbootrest/models/Employee.java
@@ -0,0 +1,16 @@
+package com.project.az.springbootrest.models;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
