20a3b12459d7fd558ae8c72fe0a49ff3e71fe566
azhwani <azh.abderrahim@gmail.com>
2019-10-14 18:37:37 +0100

    Add application.properties

--- src/main/java/com/project/az/springbootrest/models/Employee.java
+++ src/main/java/com/project/az/springbootrest/models/Employee.java
@@ -2,15 +2,58 @@ package com.project.az.springbootrest.models;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employee")
 public class Employee implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	@Column
+	private String name;
+	@Column
+	private String email;
 
 	public Employee() {
 		// TODO Auto-generated constructor stub
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
 
 }
