882dd2b8a94e4cb1e8e97ad17d64a676d2625518
azhwani <azh.abderrahim@gmail.com>
2019-10-15 18:30:06 +0100

    Service layer Unit Test V2

---
+++ src/test/java/com/project/az/springbootrest/SpringBootApplicationTestServicesV2.java
@@ -0,0 +1,61 @@
+package com.project.az.springbootrest;
+
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.project.az.springbootrest.dao.EmployeeRepository;
+import com.project.az.springbootrest.models.Employee;
+import com.project.az.springbootrest.services.EmployeeService;
+import com.project.az.springbootrest.services.IEmployeeService;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+@RunWith(SpringRunner.class)
+public class SpringBootApplicationTestServicesV2 {
+
+	@TestConfiguration
+    static class EmployeeServiceTestContextConfiguration {
+        @Bean
+        public EmployeeService employeeService() {
+            return new EmployeeService();
+        }
+    }
+ 
+    @Autowired
+	IEmployeeService empservice;
+	
+	@MockBean
+	EmployeeRepository emprep;
+	
+	@Before
+    public void init() {
+       
+		Employee emp01 = new Employee();
+		emp01.setName("Vu");
+		emp01.setEmail("abc@gmail.com");
+		Optional<Employee> emp = Optional.of(emp01);
+
+	    Mockito.when(emprep.findByName(emp01.getName())).thenReturn(emp);
+		
+    }
+	
+	@Test
+	public void testService() {
+		
+		Optional<Employee> emp = empservice.findByName("Vu");
+		assertThat(emp.get().getEmail(), equalTo("abc@gmail.com"));
+		
+	}
+	
+
+}
